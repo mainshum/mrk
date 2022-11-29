7e6f37e35c156e2c5a65b3caa3b2206147a15947
mainshum <aleksander@aims.ai>
2022-11-29 21:51:33 +0100

    Added language handling

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,10 @@
 import type { Doctor } from "@prisma/client";
 
+export const allowed = ['uk', 'en', 'pl'];
+
 export interface Specialization {
   specName: string;
   drs: Doctor[];
 }
+
+export type Lang = 'uk' | 'en' | 'pl';
