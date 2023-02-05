444052c9ab9fcec55fda721094d9c8ec4186fb4d
mainshum <aleksander@aims.ai>
2023-02-05 10:55:12 +0100

    Dodano francuska pestke

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,10 @@
 import type { Doctor } from "@prisma/client";
 
-export const allowed = ['pl', 'en', 'ua'];
+export const allowed = ['pl', 'en', 'ua', 'fr'];
 
 export interface Specialization {
   specName: string;
   drs: Doctor[];
 }
 
-export type Lang = 'ua' | 'en' | 'pl';
+export type Lang = 'ua' | 'en' | 'pl' | 'fr';
